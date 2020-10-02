62e0d1d20403a553e0518d32b7c81cc07d5da4b9
aniket786333 <71198155+aniket786333@users.noreply.github.com>
2020-10-02 05:41:08 +0530

    Main show same notion.py

--- python/main.py
+++ python/main.py
@@ -1,20 +1,20 @@
-# Copyright  2020  Edoardo Riggio
+# Copyright  2020  Edoardo Riggio.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain a copy of the License at,
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
+# Unless required by applicable law or agreed to in writing, software,
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
+# WITHOUT WARRANTIES OR CONDITIONS FOR ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and,
 # limitations under the License.
 
-# Local scripts
-from get_members import get_members
-from email_generator import generate_graphs
+# Local scripts.
+from get_members import get_members,
+from email_generator import generate_graphs.
 
 if __name__ == "__main__":
     subscribers = get_members()
